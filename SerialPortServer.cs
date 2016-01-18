b57c5225d831d78424e2c68287385baf24574997
Jonathan Melcher <jmelcher@ualberta.ca>
2016-01-17 20:34:47 -0700

    Separated Read/Write into interfaces, turned SerialServer into abstract class, RS232 Server replaces old SerialServer

---
+++ SerialPortServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Capstone
+{
+    abstract class SerialPortServer : ISerialWritable, ISerialReadable
+    {
+        protected const int DEFAULT_TIMEOUT_MS = 1000;
+        protected volatile bool _isRunning;
+
+        protected SerialPortServer(SerialPortSettings sps)
+        {
+            Port = new SerialPort(sps.PortName, sps.BaudRate, sps.Parity, sps.DataBits, sps.StopBits);
+            Port.ReadTimeout = DEFAULT_TIMEOUT_MS;
+            Port.WriteTimeout = DEFAULT_TIMEOUT_MS;
+        }
+
+        protected SerialPort Port { get; set; }
+        protected Thread Reader { get; set; }
+        protected Thread Writer { get; set; }
+
+        public abstract void Start();
+        public abstract void Stop();
+
+        public virtual void Write(byte b) { }
+        public virtual void Write(byte[] arr) { }
+        public virtual byte Read() { }
+    }
+}
---
+++ ThreadSafeQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Capstone
+{
+    public class ThreadSafeQueue<T>
+    {
+        private object _syncRoot;
+        private Queue<T> _q;
+
+        public ThreadSafeQueue()
+        {
+            _syncRoot = new object();
+            _q = new Queue<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            lock (_syncRoot)
+                _q.Enqueue(item);
+        }
+
+        public T Dequeue()
+        {
+            lock (_syncRoot)
+                return _q.Dequeue();
+        }
+
+        public int Count
+        {
+            get
+            {
+                lock (_syncRoot)
+                    return _q.Count;
+            }
+        }
+    }
+}
