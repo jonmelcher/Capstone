7a1ab4668f6e6edab3f83a147c0d3974da287aa5
Jonathan Melcher <jmelcher@ualberta.ca>
2016-03-17 18:25:47 -0600

    updated libraries

---
+++ linearActuatorHorizontalPortK.h
@@ -0,0 +1,33 @@
+// ****************************************************************
+//  filename    :   linearActuatorHorizontalPortK.h
+//  purpose     :   header for linearActuatorHorizontalPortK.c
+//
+//  written by Jonathan Melcher and Brennan MacGregor on 2016/03/15
+// ****************************************************************
+
+#ifndef _HIDEF_H_
+#define _HIDEF_H_
+#include <hidef.h>
+#endif
+
+#ifndef _DERIVATIVE_H_
+#define _DERIVATIVE_H_
+#include "derivative.h"
+#endif
+
+#ifndef _LINEAR_ACTUATOR_HORIZONTAL_PORTK_H_
+#define _LINEAR_ACTUATOR_HORIZONTAL_PORTK_H_
+
+typedef struct LinearActuatorHorizontalPortK {
+    unsigned char isOn;
+    unsigned char isRetracted;
+    unsigned char isRetracting;
+} HorizontalActuatorK;
+
+void horizontal_actuator_init(HorizontalActuatorK* a);
+void horizontal_actuator_sync(HorizontalActuatorK* a);
+void horizontal_actuator_disable(HorizontalActuatorK* a);
+void horizontal_actuator_retract(HorizontalActuatorK* a);
+void horizontal_actuator_extend(HorizontalActuatorK* a);
+
+#endif
