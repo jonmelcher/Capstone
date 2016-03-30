a5c13f840dc00544d9b81df71c473ac677dc68c4
Jonathan Melcher <jmelcher@ualberta.ca>
2016-03-29 19:59:24 -0600

    Update GarageAssignment.cs

--- GarageAssignment.cs
+++ GarageAssignment.cs
@@ -2,25 +2,25 @@ namespace Capstone
 {
     public class GarageAssignment
     {
-      private readonly string _id;
-      private readonly int _tier;
-      private readonly int _cell;
-      private readonly bool _stored;
-    
-      public GarageAssignment(string id, bool stored, int tier, int cell)
-      {
-          _id = id;
-          _stored = stored;
-          _tier = tier;
-          _cell = cell;
-      }
-    
-      public GarageAssignment(GarageAssignment assignment, bool stored) :
-          this(assignment.ID, stored, assignment.Tier, assignment.Cell) { }
-    
-      public string ID => _id;
-      public bool Stored => _stored;
-      public int Tier => _tier;
-      public int Cell => _cell;
-    }
+        private readonly string _id;
+        private readonly int _tier;
+        private readonly int _cell;
+        private readonly bool _stored;
+        
+        public GarageAssignment(string id, bool stored, int tier, int cell)
+        {
+            _id = id;
+            _stored = stored;
+            _tier = tier;
+            _cell = cell;
+        }
+        
+        public GarageAssignment(GarageAssignment assignment, bool stored) :
+            this(assignment.ID, stored, assignment.Tier, assignment.Cell) { }
+        
+            public string ID => _id;
+            public bool Stored => _stored;
+            public int Tier => _tier;
+            public int Cell => _cell;
+        }
 }
