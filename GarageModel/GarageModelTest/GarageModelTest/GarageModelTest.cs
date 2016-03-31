d0dc4a5d1b862ad39b4dc0fffadf355ca6d893f8
Jonathan Melcher <jmelcher@ualberta.ca>
2016-03-30 19:59:41 -0600

    added GarageModelTest

---
+++ GarageModel/GarageModelTest/GarageModelTest/GarageModelTest.cs
@@ -0,0 +1,28 @@
+﻿// **************************************************************************
+//  filename    :   GarageModelTest.cs
+//  purpose     :   test out application - database functionality through the
+//                  GarageRepository
+//
+//  written by Jonathan Melcher and Brennan MacGregor on 2016-03-30
+// **************************************************************************
+
+
+using System;
+using GarageModel;
+
+
+namespace GarageModelTest
+{
+    class GarageModelTest
+    {
+        static void Main(string[] args)
+        {
+            GarageAssignment assignment = GarageRepository.GetGarageAssignment("39009D3D68");
+            Console.WriteLine($"{assignment.ID}, {assignment.Stored}, {assignment.Tier}, {assignment.Cell}");
+            GarageRepository.MoveVehicle("id", true);
+            assignment = GarageRepository.GetGarageAssignment("39009D3D68");
+            Console.WriteLine($"{assignment.ID}, {assignment.Stored}, {assignment.Tier}, {assignment.Cell}");
+            Console.ReadKey();
+        }
+    }
+}
