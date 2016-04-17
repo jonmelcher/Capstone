c0406ed2de37d8dbea71423588ea0b4b16d00b3f
Jonathan Melcher <jmelcher@ualberta.ca>
2016-04-17 11:01:56 -0600

    Update MediatorProcessingState.cs

--- GarageMediator/GarageMediator/MediatorProcessingState.cs
+++ GarageMediator/GarageMediator/MediatorProcessingState.cs
@@ -24,9 +24,9 @@ namespace GarageMediator
     {
         // communication protocol to the microcontroller
         private const byte START_INSTRUCTION = 0xF0;
-        private const byte CONTINUE_INSTRUCTION = 0xF1;
-        private const byte STOP_INSTRUCTION = 0xF2;
-        private const byte INSTRUCTIONS_COMPLETED = 0xF3;
+        private const byte CONTINUE_INSTRUCTION = 0x01;
+        private const byte STOP_INSTRUCTION = 0x02;
+        private const byte INSTRUCTIONS_COMPLETED = 0x03;
         private const byte COMMUNICATION_DELAY_MS = 250;
         private const byte OUTGOING = 0x20;
         private const byte INCOMING = 0x21;
