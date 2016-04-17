d677d4a81cc7ef40159a7fd43cadb95f008e444e
Jonathan Melcher <jmelcher@ualberta.ca>
2016-04-17 11:00:06 -0600

    Added files via upload

--- FinalizedLibraries/Main/main.c
+++ FinalizedLibraries/Main/main.c
@@ -5,17 +5,25 @@
 #include "garageCommunication.h"
 #include "garage.h"
 
-
 static const unsigned char TIERS = 3;
 static const unsigned char START_INSTRUCTION = 0xF0;
+static const unsigned char HOME = 0;
+static const unsigned char CELLS_PER_TIER = 8;
+static const unsigned long int DEGREES_BETWEEN_CELLS = 45;
+static const unsigned char OUTGOING = 0x20;
+static const unsigned char INCOMING = 0x21;
+static const unsigned long long int DELAY_BETWEEN_MOVEMENTS_MS = 500;
+static const unsigned char CONTINUE_INSTRUCTION = 0x01;
+static const unsigned char STOP_INSTRUCTION = 0x02;
+static const unsigned char INSTRUCTIONS_COMPLETED = 0x03;
+
 
 void main(void) {
 
-    StepperPortA rotational;                        // 5V 6-lead unipolar stepper motor interface
+    StepperA rotational;                            // 5V 6-lead unipolar stepper motor interface
     HorizontalActuatorK horizontal;                 // 12V bipolar 4" linear actuator interface
     VerticalActuatorA vertical;                     // 12V bipolar 12" linear actuator interface
     Instruction instruction;                        // structure for maintaining instructions from C# app
-    unsigned char rx;                               // last received datum
 
     _DISABLE_COP();
     timer_init_8us();                               // initialize enhanced capture timer
@@ -25,7 +33,6 @@ void main(void) {
     vertical_actuator_init(&vertical, TIERS);       // initalize vertical linear actuator interface
 
     for(;;) {
-
         if (!is_garage_rx_ready() || garage_rx() != START_INSTRUCTION)
             continue;
 
